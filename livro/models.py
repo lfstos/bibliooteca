7ef55ef1a57c1f0430b1d7081e9d1b46bfe62657
Luiz Fernando <luiz-fernando@outlook.com>
2023-04-01 11:30:29 -0300

    Criado app livro

---
+++ livro/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from datetime import date
+
+# Create your models here.
+
+
+class Livros(models.Model):
+    nome = models.CharField(max_length=100)
+    autor = models.CharField(max_length=30)
+    co_autor = models.CharField(max_length=30, null=True, blank=True)
+    data_cadastro = models.DateField(default=date.today)
+    emprestado = models.BooleanField(default=False)
+    nome_emprestado = models.CharField(max_length=30, blank=True)
+    data_emprestimo = models.DateTimeField(null=True, blank=True)
+    data_devolucao = models.DateTimeField(null=True, blank=True)
+    tempo_duracao = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Livro'
+
+    def __str__(self) -> str:
+        return self.nome
