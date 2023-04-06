426a64f8d70d7a09476fd2782fe397a2dae879c4
Luiz Fernando <luiz-fernando@outlook.com>
2023-04-06 18:50:17 -0300

    Alterado sistema de validação do login  e do validar_login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Usuario
-import hashlib
+from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth import authenticate, login as log
+from django.http import HttpResponse
+
 # Create your views here.
 
 
 def login(request):
-    status = request.GET.get('status')
-    return render(request, 'login.html', {'status': status})
+    return render(request, 'registration/login.html')
 
 
 def cadastro(request):
@@ -16,40 +17,39 @@ def cadastro(request):
 
 
 def valida_cadastro(request):
-    nome = request.POST.get('nome')
+    nome = request.POST.get('username')
     email = request.POST.get('email')
-    senha = request.POST.get('senha')
+    senha = request.POST.get('password')
 
-    usuario = Usuario.objects.filter(email=email)
-
-    if len(nome.strip()) == 0 or len(email.strip()) == 0:
-        return redirect('/auth/cadastro/?status=1')
-
-    if len(senha) < 8:
-        return redirect('/auth/cadastro/?status=2')
-
-    if len(usuario) > 0:
-        return redirect('/auth/cadastro/?status=3')
     try:
-        senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
-        usuario = Usuario(nome=nome, email=email, senha=senha)
+        user = get_object_or_404(Usuario, email=email)
+        return redirect('/auth/cadastro/?status=3')
+    except:
+        if len(nome.strip()) == 0 or len(email.strip()) == 0:
+            return redirect('/auth/cadastro/?status=1')
+        if len(senha) < 8:
+            return redirect('/auth/cadastro/?status=2')
+        usuario = Usuario()
+        usuario.username = nome
+        usuario.email = email
+        usuario.password = make_password(senha)
         usuario.save()
         return redirect('/auth/cadastro/?status=0')
-    except:
-        return redirect('/auth/cadastro/?status=4')
 
 
 def valida_login(request):
     email = request.POST.get('email')
-    senha = request.POST.get('senha')
+    senha = request.POST.get('password')
 
-    usuario = Usuario.objects.filter(email=email, senha=senha)
+    user = authenticate(request, email=email, password=senha)
 
-    if len(usuario) == 0:
+    if user:
+        log(request, user)
+        request.session['usuario'] = user.id
+        return redirect(f'/livro/home/?id_usuario={request.session["usuario"]}', {'user': user})
+
+    else:
         return redirect('/auth/login/?status=1')
-    elif len(usuario) > 0:
-        request.session['usuario'] = usuario[0].id
-        return redirect(f'/livro/home/?id_usuario={request.session["usuario"]}')
 
 
 def sair(request):
