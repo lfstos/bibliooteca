ca7824b4623a4c54f08c4f19a8a7b5e4b2b226b8
Luiz Fernando <luiz-fernando@outlook.com>
2023-04-06 18:40:53 -0300

    Adidiona Class Categoria e foreign Keys categoria e usuario

--- livro/models.py
+++ livro/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from datetime import date
-
+from usuarios.models import Usuario
 # Create your models here.
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=30)
+    descricao = models.TextField()
+
+    def __str__(self) -> str:
+        return self.nome
 
 
 class Livros(models.Model):
@@ -14,6 +22,8 @@ class Livros(models.Model):
     data_emprestimo = models.DateTimeField(null=True, blank=True)
     data_devolucao = models.DateTimeField(null=True, blank=True)
     tempo_duracao = models.DateTimeField(null=True, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
 
     class Meta:
         verbose_name = 'Livro'
