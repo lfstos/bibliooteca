71bad8dd744fe40fc3d85f88fb23f1e6bcc33435
Luiz Fernando <luiz-fernando@outlook.com>
2023-04-10 16:36:28 -0300

    Criado lógica para mostrar lista de livro emprestados

--- livro/views.py
+++ livro/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from usuarios.models import Usuario
-from .models import Livros
+from .models import Livros, Categoria, Emprestimos
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-
-# Create your views here.
 
 
-@login_required(login_url='/auth/login/')
+@login_required(login_url='/auth/login/?status=2')
 def home(request):
     id = request.session['usuario']
     livros = Livros.objects.filter(usuario_id=id)
     return render(request, 'home.html', {'livros': livros})
 
-    
-@login_required(login_url='/auth/login/')
+
+@login_required(login_url='/auth/login/?status=2')
 def ver_livro(request, id):
     livro = Livros.objects.get(id=id)
 
-    return render(request, 'ver_livro.html', {'livro': livro})
+    if request.session.get('usuario') == livro.usuario_id:
+        categorias = Categoria.objects.filter(usuario_id=request.session.get('usuario'))
+        emprestimos = Emprestimos.objects.filter(livro=livro)
+        context = {
+            'livro': livro, 
+            'categorias': categorias, 
+            'emprestimos': emprestimos
+        }
+        return render(request, 'ver_livro.html', context)
+    else:
+        livro = 'Não existe id de livro para esse usuário'
+        return render(request, 'ver_livro.html', {'livro': livro})
