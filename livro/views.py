09e1a13d0bdc9573012a968ad6cfa39ebcc8597b
Luiz Fernando <luiz-fernando@outlook.com>
2023-04-11 20:43:18 -0300

    Altertada função home, inclui um CadastroLivroForm para criar o formulário de cadastro de livro, função ver_livro e Criada funçção cadastrar_livro

--- livro/views.py
+++ livro/views.py
@@ -3,13 +3,21 @@ from django.http import HttpResponse
 from usuarios.models import Usuario
 from .models import Livros, Categoria, Emprestimos
 from django.contrib.auth.decorators import login_required
+from .forms import CadastroLivroForm
 
 
 @login_required(login_url='/auth/login/?status=2')
 def home(request):
     id = request.session['usuario']
     livros = Livros.objects.filter(usuario_id=id)
-    return render(request, 'home.html', {'livros': livros})
+    form = CadastroLivroForm()
+    context = {
+        'livros': livros,
+        'usuario_logado': request.session['usuario'],
+        'form': form
+    }
+
+    return render(request, 'home.html', context)
 
 
 @login_required(login_url='/auth/login/?status=2')
@@ -17,14 +25,26 @@ def ver_livro(request, id):
     livro = Livros.objects.get(id=id)
 
     if request.session.get('usuario') == livro.usuario_id:
-        categorias = Categoria.objects.filter(usuario_id=request.session.get('usuario'))
+        categorias = Categoria.objects.filter(
+            usuario_id=request.session.get('usuario'))
         emprestimos = Emprestimos.objects.filter(livro=livro)
         context = {
-            'livro': livro, 
-            'categorias': categorias, 
-            'emprestimos': emprestimos
+            'livro': livro,
+            'categorias': categorias,
+            'emprestimos': emprestimos,
+            'usuario_logado': request.session['usuario']
         }
         return render(request, 'ver_livro.html', context)
     else:
-        livro = 'Não existe id de livro para esse usuário'
+        livro = 'Não existe esse livro para esse usuário'
         return render(request, 'ver_livro.html', {'livro': livro})
+
+
+@login_required(login_url='/auth/login/?status=2')
+def cadastrar_livro(request):
+    if request.method == 'POST':
+        form = CadastroLivroForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponse(form)
+    return HttpResponse('GET')
