bc191cd7ca4455dbc2ae37cd25a3f6d2465a49d1
Luiz Fernando <luiz-fernando@outlook.com>
2023-04-12 21:37:23 -0300

    Adicionado campo usuário hidden

--- livro/forms.py
+++ livro/forms.py
@@ -1,17 +1,18 @@
 from django import forms
-from .models import Livros
+from .models import Livros, Categoria
 
 
 class CadastroLivroForm(forms.ModelForm):
     class Meta:
         model = Livros
-        fields = ('nome', 'autor', 'co_autor',
-                  'data_cadastro', 'emprestado', 'categoria')
+        fields = ('nome', 'autor', 'co_autor', 'data_cadastro', 
+                  'emprestado', 'categoria', 'usuario')
         widgets = {
             'nome': forms.TextInput(attrs={'class': 'form-control', 'style': 'margin-bottom: 10px;'}),
             'autor': forms.TextInput(attrs={'class': 'form-control', 'style': 'margin-bottom: 10px;'}),
             'co_autor': forms.TextInput(attrs={'class': 'form-control', 'style': 'margin-bottom: 10px;'}),
             'data_cadastro': forms.TextInput(attrs={'class': 'form-control', 'style': 'margin-bottom: 10px;'}),
             'emprestimo': forms.TextInput(attrs={'class': 'form-control', 'style': 'margin-bottom: 10px;'}),
-            'categoria': forms.Select(attrs={'class': 'form-control', 'style': 'margin-bottom: 10px;'})
+            'categoria': forms.Select(attrs={'class': 'form-control', 'style': 'margin-bottom: 10px;'}),
+            'usuario': forms.HiddenInput()
         }
