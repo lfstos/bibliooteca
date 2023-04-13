d7d0ba1a5abe0469ee3e161800efb7465268dbe4
Luiz Fernando <luiz-fernando@outlook.com>
2023-04-12 21:41:26 -0300

    Criada url para exluir livro

--- livro/urls.py
+++ livro/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('ver_livro/<int:id>', views.ver_livro, name='ver_livro'),
     path('cadastrar_livro/', views.cadastrar_livro, name='cadastrar_livro'),
+    path('excluir_livro/<int:id>', views.excluir_livro, name='excluir_livro')
 ]
